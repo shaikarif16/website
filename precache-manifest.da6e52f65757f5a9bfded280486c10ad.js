62f075f5137c8be91fe1340ec4616c6eff90efe7
Arif Shaik <shaikarif00207@gmal.com>
2020-09-17 18:59:21 +0530

    Updates

---
+++ precache-manifest.da6e52f65757f5a9bfded280486c10ad.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "985a54e303a9c2d17fe2ebdc0154af48",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "827b5f1f86c82714a642",
+    "url": "/website/static/css/2.ea6b7726.chunk.css"
+  },
+  {
+    "revision": "15ed2a4a2b69a42cfd86",
+    "url": "/website/static/css/main.7c73c9e9.chunk.css"
+  },
+  {
+    "revision": "827b5f1f86c82714a642",
+    "url": "/website/static/js/2.d48319e0.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/website/static/js/2.d48319e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15ed2a4a2b69a42cfd86",
+    "url": "/website/static/js/main.1df6e2cb.chunk.js"
+  },
+  {
+    "revision": "9c23e00f99dd7916e537",
+    "url": "/website/static/js/runtime-main.9b825fe6.js"
+  },
+  {
+    "revision": "27cceb785dee7b21d2c3db739443ea7c",
+    "url": "/website/static/media/android.27cceb78.jpg"
+  },
+  {
+    "revision": "65dcd200122c5a8637da031e9b61b44e",
+    "url": "/website/static/media/app.65dcd200.jpg"
+  },
+  {
+    "revision": "dbe00b0f1be1cc8ec2826d9ee93b40fe",
+    "url": "/website/static/media/digital.dbe00b0f.jpg"
+  },
+  {
+    "revision": "f23ab7a66d8d507f384d9b3db68267ad",
+    "url": "/website/static/media/img2.f23ab7a6.jpg"
+  },
+  {
+    "revision": "738e716221c7968215e65be193a453f4",
+    "url": "/website/static/media/marketing.738e7162.jpg"
+  },
+  {
+    "revision": "e98a33a1576306b65eb79e3d4c735c08",
+    "url": "/website/static/media/s1.e98a33a1.jpg"
+  },
+  {
+    "revision": "9eb70d38043f844689553033af73148c",
+    "url": "/website/static/media/software.9eb70d38.jpg"
+  },
+  {
+    "revision": "8a3e0e571c7ff5b9d2ca38282934e1e4",
+    "url": "/website/static/media/web.8a3e0e57.jpg"
+  }
+]);
